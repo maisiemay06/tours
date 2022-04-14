2746fc91cce42af4e9e62e16a0b56347fadfcb67
maisiemay06 <73712593+maisiemay06@users.noreply.github.com>
2022-04-14 12:10:42 +0100

    add tour info, remove and refresh buttons

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,23 @@
 import React, { useState } from 'react';
 
-export default function Tour() {
-  return <h2>tour component</h2>;
+export default function Tour({id, name, info, image, price, removeTour}) {
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <article className='single-tour'>
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">£{price}</h4>
+        </div>
+        <p>
+          {showMore ? info : `${info.substring(0,200)}...`}
+          <button onClick={() => setShowMore(!showMore)}>{showMore?`show less`: `read more`}</button>
+        </p>
+        <button className="delete-btn" onClick={() => removeTour(id)}>not interested</button>
+      </footer>
+    </article>
+  );
 };
 
--- src/Tours.js
+++ src/Tours.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import Tour from './Tour';
 
-export default function Tours() {
-  return <h2>tours component</h2>;
+export default function Tours({tours, removeTour}) {
+  return (
+    <section>
+      <div className="title">
+        <h2>Our tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour}/>
+        })}
+      </div>
+    </section>
+  );
 };
